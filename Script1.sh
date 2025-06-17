4120ce0745d18d71d2f337feee614defb3811b0b
Swaraj@0523 <jaiswalswati8999@gmail.com>
2025-06-17 22:52:18 +0530

    i am commiting first time

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,5 @@ echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
 echo "I am learning branching in git" 
 echo "I am learnig git and github"
-echo "I am Swati"
+echo "I am Swati Rahul Jaiswal"
 
