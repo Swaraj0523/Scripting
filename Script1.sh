1a760175c3ea30d8eea1b0a8dbafcc0795a21da0
Swaraj@0523 <jaiswalswati8999@gmail.com>
2025-06-17 22:19:56 +0530

    this is my first commit

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,7 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
+echo "I am learning branching in git" 
+echo "I am learnig git and github"
+echo "I am Swati"
 
